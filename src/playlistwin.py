785325a7f19a9793191a5e5b2f3acf1f8f8ba4d6
Jörg Lehmann <joerg-lehmann@users.noreply.github.com>
2006-09-02 22:09:11 +0000

    more encoding

--- src/playlistwin.py
+++ src/playlistwin.py
@@ -1,6 +1,6 @@
 # -*- coding: ISO-8859-1 -*-
 
-# Copyright (C) 2002, 2003, 2004 Jörg Lehmann <joerg@luga.de>
+# Copyright (C) 2002, 2003, 2004, 2006 Jörg Lehmann <joerg@luga.de>
 #
 # This file is part of PyTone (http://www.luga.de/pytone/)
 #
@@ -24,6 +24,7 @@ import events, hub
 import playlist
 import statusbar
 import window
+import encoding
 
 from helper import formattime
 
@@ -190,7 +191,7 @@ class playlistwin(window.window):
                 adddict = {"playstarthours":   h,
                            "playstartminutes": m,
                            "playstartseconds": s}
-                name = item.song.format(self.songformat, adddict=adddict)
+                name = encoding.encode(item.song.format(self.songformat, adddict=adddict))
                 if self.playlist.playingitem and item is self.playlist.playingitem:
                     if self.playlist.selected==i and self.hasfocus():
                         attr = self.colors.selected_playingsong
