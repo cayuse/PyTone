0493e008177643e3a0e5bf135908931310acad8c
Jörg Lehmann <joerg-lehmann@users.noreply.github.com>
2006-09-16 12:41:35 +0000

    make message windows bigger

--- src/messagewin.py
+++ src/messagewin.py
@@ -57,7 +57,7 @@ class messagewin(window.window):
         if self.maxw<=80:
             width = self.maxw
         else:
-            width = 80 + int((self.maxw-80)*0.5)
+            width = 80 + int((self.maxw-80)*0.8)
         height = min(self._outputlen(width-2)+2, self.maxh-3)
         y = max(0, (self.maxh-height)/2)
         x = max(0, (self.maxw-width)/2)
